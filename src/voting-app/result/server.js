4b813f5d325c33a3ff68a0a3ba9f384a859aa9a4
echoamir <kolahiamir1@gmail.com>
2025-12-27 10:02:26 +0330

    uptate gitlab-ci file and source code

--- src/voting-app/result/server.js
+++ src/voting-app/result/server.js
@@ -1,62 +1,58 @@
 var express = require('express'),
-    async = require('async'),
     { Pool } = require('pg'),
     cookieParser = require('cookie-parser'),
     app = express(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io')(server),
+    path = require('path');
 
 var port = process.env.PORT || 4000;
 
 io.on('connection', function (socket) {
-
-  socket.emit('message', { text : 'Welcome!' });
+  socket.emit('message', { text: 'Welcome!' });
 
   socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
 });
 
-var pool = new Pool({
-  connectionString: 'postgres://postgres:adminpassword@pgpool/adminpassword'
+const pool = new Pool({
+  host: 'pgpool',
+  port: 5432,
+  user: 'postgres',
+  password: 'adminpassword',
+  database: 'postgres',
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 });
 
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
-    });
-  },
-  function(err, client) {
-    if (err) {
-      return console.error("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
+pool.on('error', (err) => {
+  console.error('Unexpected PG pool error', err);
+});
+
+async function getVotes() {
+  try {
+    const result = await pool.query(
+      'SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote'
+    );
+
+    const votes = collectVotesFromResult(result);
+    io.sockets.emit("scores", JSON.stringify(votes));
+
+  } catch (err) {
+    console.error('DB query failed:', err.message);
   }
-);
 
-function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
-
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
+  setTimeout(getVotes, 1000);
 }
 
-function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+getVotes(); // 
 
-  result.rows.forEach(function (row) {
+function collectVotesFromResult(result) {
+  var votes = { a: 0, b: 0 };
+
+  result.rows.forEach(row => {
     votes[row.vote] = parseInt(row.count);
   });
 
@@ -64,7 +60,7 @@ function collectVotesFromResult(result) {
 }
 
 app.use(cookieParser());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
@@ -72,6 +68,6 @@ app.get('/', function (req, res) {
 });
 
 server.listen(port, function () {
-  var port = server.address().port;
   console.log('App running on port ' + port);
 });
+
